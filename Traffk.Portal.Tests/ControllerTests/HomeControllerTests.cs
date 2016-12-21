ebd736570ad2c720d70ab02176b0af7af0c2d74d
Darren Alfonso <darren@traffk.com>
2016-12-20 16:18:12 -0800

    #26 - Add Test Project for Traffk.Portal and 1 dummy test

---
+++ Traffk.Portal.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Traffk.Portal;
+
+namespace Traffk.Portal.Tests.ControllerTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestClass]
+        public class IndexMethodTests
+        {
+            [TestMethod]
+            public void WhenGivenIndexReturnNotNull()
+            {
+                var testString = "NOT NULL";
+                Assert.IsNotNull(testString);
+            }
+        }
+    }
+}
---
+++ Traffk.Portal.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Traffk.Portal.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6efb4b65-2349-4686-89aa-71a1036af822")]
