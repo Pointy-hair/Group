7e1340efc66450708695853b0ce24483df64c7b4
Darren Alfonso <darren@traffk.com>
2016-12-21 10:41:55 -0800

    #26 - Add dummy test for Traffk.Bal

---
+++ Traffk.Bal.Tests/EmailTests/RawEmailerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Traffk.Bal.Email;
+
+namespace Traffk.Bal.Tests.EmailTests
+{
+    [TestClass]
+    public class RawEmailerTests
+    {
+        [TestClass]
+        public class SendEmailMethodTests
+        {
+            [TestMethod]
+            public void WhenGivenSmtpOptionsSendEmail()
+            {
+                var testString = "NOT NULL";
+                Assert.IsNotNull(testString);
+
+                //object nullObject = null;
+                //Assert.IsNotNull(nullObject);
+            }
+        }
+    }
+}
