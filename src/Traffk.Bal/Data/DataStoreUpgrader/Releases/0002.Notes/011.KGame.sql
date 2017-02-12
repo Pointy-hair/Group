3878286d982572528f07749b4f1144961bf72d73
jason <jason@traffk.com>
2017-02-12 10:23:05 -0800

    Setup a release notes file for the K sprint

---
+++ src/Traffk.Bal/Data/DataStoreUpgrader/Releases/0002.Notes/011.KGame.sql
@@ -0,0 +1,18 @@
+﻿insert into Releases values ('urn:traffk.com/portal', '2017-02-19', 'K Game', null)
+
+GO
+
+insert into ReleaseChanges
+(ReleaseId, ChangeType, Title)
+select r.ReleaseId, a.ChangeType, a.Title
+from
+(
+	select 'new' ChangeType, 'Example New Item' Title
+	union all
+	select 'fix' ChangeType, 'Example Fix'
+) a,
+(
+	select max(ReleaseId) ReleaseId from Releases
+) r
+
+GO
