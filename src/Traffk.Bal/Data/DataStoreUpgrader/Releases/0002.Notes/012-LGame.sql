7d4ce780e9e201fa552fcb523b0b42f3f12fe7d6
Darren Alfonso <darren@traffk.com>
2017-02-22 15:05:44 -0800

    HIP-28 Add notes for L release

---
+++ src/Traffk.Bal/Data/DataStoreUpgrader/Releases/0002.Notes/012-LGame.sql
@@ -0,0 +1,23 @@
+﻿insert into Releases values ('urn:traffk.com/portal', '2017-02-19', 'Leisure Suit Larry', null)
+
+GO
+
+insert into ReleaseChanges
+(ReleaseId, ChangeType, Title)
+select r.ReleaseId, a.ChangeType, a.Title
+from
+(
+	select 'new' ChangeType, 'Change inactivity timespan to 15 minutes' Title
+	union all
+	select 'new' ChangeType, 'Convert action confirmations to Frankie UI' Title
+	union all
+	select 'new' ChangeType, 'Update navigation bar functionality' Title
+	union all
+	select 'new' ChangeType, 'Bug fixes' Title
+
+) a,
+(
+	select max(ReleaseId) ReleaseId from Releases
+) r
+
+GO
