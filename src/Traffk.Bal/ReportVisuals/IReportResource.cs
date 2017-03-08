1d1b456fc1fb45fbec2192f1a2c7c31714ce7a90
Darren Alfonso <darren@traffk.com>
2017-03-08 14:35:31 -0800

    untracked files on develop: dced81e HIP-45 Initial commit for creation of Report Metadata layer

---
+++ src/Traffk.Bal/ReportVisuals/IReportResource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traffk.Bal.ReportVisuals
+{
+    public interface IReportResource
+    {
+        string Title { get; set; }
+        string Description { get; set; }
+    }
+}
---
+++ src/Traffk.Bal/ReportVisuals/ReportVisualFolder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traffk.Bal.ReportVisuals
+{
+    public class ReportVisualFolder : IReportVisualFolder, IReportResource
+    {
+        public ReportVisualFolder(string title)
+        {
+            Title = title;
+        }
+        public string Title { get; set; }
+        string IReportResource.Title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        string IReportVisualFolder.Title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        public string Description { get; set; }
+    }
+}
