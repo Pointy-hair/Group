9de4896b002a5e388e953388e4f051650fce4bb9
Darren Alfonso <darren@traffk.com>
2017-05-29 23:11:18 -0700

    Add ITraffkRecurringJobManager.cs

---
+++ src/Traffk.Bal/BackgroundJobs/ITraffkRecurringJobManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hangfire;
+
+namespace Traffk.Bal.BackgroundJobs
+{
+    public interface ITraffkRecurringJobManager
+    {
+        string Add(Hangfire.Common.Job job, string cronExpression);
+
+        void Update(string recurringJobId, Hangfire.Common.Job job, string cronExpression);
+    }
+}
