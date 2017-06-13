2c20f98620eeb8e517492fcc6169246452bf5daa
Darren Alfonso <darren@traffk.com>
2017-06-13 14:45:38 -0700

    RX-10 Move GetApiKey link from Admin menu to Manage Index

--- src/Traffk.Portal/Models/ManageViewModels/GenerateApiKeyViewModel.cs
+++ src/Traffk.Portal/Models/ManageViewModels/GenerateApiKeyViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace Traffk.Portal.Models.ManageViewModels
 {
     public class GenerateApiKeyViewModel
     {
+        [DisplayName("API Key")]
         public string ApiKey { get; set; }
     }
 }
