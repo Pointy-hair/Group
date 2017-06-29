798f35d74740ad5a145d0ca5f9922f8981f230ca
Darren Alfonso <darren@traffk.com>
2017-06-28 18:24:56 -0700

    RX-25 Log external calls to Orchestra Api

--- src/Traffk.Bal/ExternalApis/OrchestraApiService.cs
+++ src/Traffk.Bal/ExternalApis/OrchestraApiService.cs
@@ -4,7 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
 using Traffk.Bal.Data.ApiModels.Rx;
+using Traffk.Bal.Logging;
+using Traffk.Bal.Settings;
 using Traffk.Orchestra;
 using Traffk.Orchestra.Models;
 using Drug = Traffk.Bal.Data.ApiModels.Rx.Drug;
@@ -13,12 +16,16 @@ using PharmacyResponse = Traffk.Orchestra.Models.PharmacyResponse;
 
 namespace Traffk.Bal.ExternalApis
 {
-    public class OrchestraApiService
+    public class OrchestraApiService : LoggedService
     {
         private readonly OrchestraRxApiClient Client;
         private readonly ICacher ScopedCacher;
+        private readonly EventType.LoggingEventTypes EventType = Settings.EventType.LoggingEventTypes.ExternalApiCall;
 
-        public OrchestraApiService(ICacher cache, OrchestraRxApiClient client, ITraffkTenantFinder tenantFinder)
+        public OrchestraApiService(ICacher cache, 
+            OrchestraRxApiClient client, 
+            ITraffkTenantFinder tenantFinder,
+            ILogger logger) : base(logger)
         {
             var tenantId = tenantFinder.GetTenantIdAsync().Result;
             ScopedCacher = cache.CreateScope(tenantId);
@@ -51,7 +58,7 @@ namespace Traffk.Bal.ExternalApis
         public PharmacyResponse PharmacySearch(string zip, int radius)
         {
             var cacheKey = RevolutionaryStuff.Core.Caching.Cache.CreateKey(typeof(PharmacyResponse).Name, zip, radius);
-
+            Log(EventType, OrchestraRxApiClient.OrchestraEndpoints.PharmacySearch);
             var pharmacyResponse = ScopedCacher.FindOrCreate<PharmacyResponse>(cacheKey, key => Client.PharmacySearchAsync(zip,radius).Result);
 
             return pharmacyResponse?.Value;
@@ -63,6 +70,7 @@ namespace Traffk.Bal.ExternalApis
             var tDrugResponse = ScopedCacher.FindOrCreate<Data.ApiModels.Rx.DrugResponse>(cacheKey, outerKey =>
             {
                 var searchCacheKey = RevolutionaryStuff.Core.Caching.Cache.CreateKey(typeof(DrugResponse).Name, query);
+                Log(EventType, OrchestraRxApiClient.OrchestraEndpoints.DrugSearch);
                 var oDrugResponse = ScopedCacher.FindOrCreate<DrugResponse>(searchCacheKey,
                     key => Client.DrugSearchAsync(query).Result);
                 var oDrugs = oDrugResponse.Value.Drugs.ToList();
@@ -72,6 +80,7 @@ namespace Traffk.Bal.ExternalApis
                 Parallel.ForEach(oDrugs, (oDrug) =>
                     {
                         var tDrug = new Drug(oDrug);
+                        Log(EventType, OrchestraRxApiClient.OrchestraEndpoints.DrugDetail);
                         var oDrugDetail = ScopedCacher.FindOrCreate<DrugDetailResponse>((oDrug.DrugID.ToString()),
                             key => Client.DrugDetailAsync(oDrug.DrugID.ToString()).ExecuteSynchronously());
                         var oDosages = oDrugDetail.Value.Dosages;
@@ -90,6 +99,7 @@ namespace Traffk.Bal.ExternalApis
         public DrugDetailResponse DrugDetail(string ndcOrOrchestraId)
         {
             var cacheKey = RevolutionaryStuff.Core.Caching.Cache.CreateKey(typeof(DrugDetailResponse).Name, ndcOrOrchestraId);
+            Log(EventType, OrchestraRxApiClient.OrchestraEndpoints.DrugDetail);
             var drugDetailResponse = ScopedCacher.FindOrCreate<DrugDetailResponse>(cacheKey, key => Client.DrugDetailAsync(ndcOrOrchestraId).Result);
             return drugDetailResponse?.Value;
         }
@@ -97,6 +107,7 @@ namespace Traffk.Bal.ExternalApis
         public DrugOption[] DrugAlternativeSearch(IEnumerable<DrugAlternativeSearchQuery> searchQuery)
         {
             var queryCacheKey = RevolutionaryStuff.Core.Caching.Cache.CreateKey(typeof(DrugOption[]).Name, searchQuery);
+            Log(EventType, OrchestraRxApiClient.OrchestraEndpoints.DrugAlternatives);
             var drugOptions = ScopedCacher.FindOrCreate<DrugOption[]>(queryCacheKey,
                 key => Client.DrugAlternativeMultipleSearchAsync(searchQuery).ExecuteSynchronously());
             return drugOptions.Value;
---
+++ src/Traffk.Bal/Logging/LoggedService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog;
+using Serilog.Core;
+using Serilog.Core.Enrichers;
+using Traffk.Bal.Settings;
+
+namespace Traffk.Bal.Logging
+{
+    public abstract class LoggedService
+    {
+        protected readonly ILogger Logger;
+
+        protected LoggedService(ILogger logger)
+        {
+            Logger = logger.ForContext(new ILogEventEnricher[]
+            {
+                new PropertyEnricher(typeof(Type).Name, this.GetType().Name),
+            });
+        }
+
+        protected void Log(EventType.LoggingEventTypes eventType = EventType.LoggingEventTypes.Default, string message = null)
+        {
+            Logger.Information("{@EventType} " + (message ?? ""), eventType.ToString() ?? "");
+        }
+    }
+}
