004e938caff234ad841a9407d5337cad579b5334
Jason <jason@jasonthomas.com>
2017-07-30 21:57:29 -0400

    More zip code reference data

---
+++ src/Traffk.Databases/ReferenceData/EtlSampleData/ZipCodes/_databases_zip4/zip-4-deluxe-MYSQL.sql
@@ -0,0 +1,66 @@
+/*
+------------------------------------------------------------------
+Provider:  Zip-Codes.com
+Product:   U.S. ZIP +4 Database Deluxe
+------------------------------------------------------------------
+This SQL Creates a new table named ZIP4, 
+related indexes, and extended column information.
+
+This script is designed to work with MySQL.
+
+Actions:
+  1.) Drop Table ZIP4 if it exists
+  2.) Creates Table named ZIP4
+
+Last Updated: 7/1/2012
+------------------------------------------------------------------
+*/
+
+
+/* 1.) Drop Table if it Exists */
+DROP TABLE IF EXISTS ZIP4;
+
+
+/* 2.) Create Table */
+CREATE TABLE ZIP4 (
+	ZipCode char(5),
+	UpdateKey varchar(10),
+	Action char(1),
+	RecordType varchar(1) NULL,
+	CarrierRoute varchar(4) NULL,
+	StPreDirAbbr varchar(2) NULL,
+	StName varchar(28) NULL,
+	StSuffixAbbr varchar(4) NULL,
+	StPostDirAbbr varchar(2) NULL,
+	AddressPrimaryLowNumber varchar(10) NULL,
+	AddressPrimaryHighNumber varchar(10) NULL,
+	AddressPrimaryOddEven varchar(1) NULL,
+	BuildingName varchar(40) NULL,
+	AddressSecAbbr varchar(4) NULL,
+	AddressSecLowNumber varchar(10) NULL,
+	AddressSecHighNumber varchar(10) NULL,
+	AddressSecOddEven varchar(1) NULL,
+	Plus4Low varchar(4) NULL,
+	Plus4High varchar(4) NULL,
+	BaseAlternateCode varchar(1) NULL,
+	LACSStatus varchar(1) NULL,
+	GovernmentBuilding varchar(1) NULL,
+	FinanceNumber varchar(6) NULL,
+	State varchar(2) NULL,
+	CountyFIPS varchar(3) NULL,
+	CongressionalDistrict varchar(2) NULL,
+	MunicipalityKey varchar(6) NULL,
+	UrbanizationKey varchar(6) NULL,
+	PreferredLastLineKey varchar(6) NULL,
+	ToLatitude decimal(18, 10) NULL,
+	FromLatitude decimal(18, 10) NULL,
+	ToLongitude decimal(18, 10) NULL,
+	FromLongitude decimal(18, 10) NULL,
+	CensusTract varchar(15) NULL,
+	CensusBlock varchar(15) NULL,
+	TLID varchar(15) NULL,
+	LatLonMultiMatch varchar(1) NULL,
+	CenLat decimal(18, 10) NULL,
+	CenLon decimal(18, 10) NULL,
+	BlockGroup varchar(15) NULL
+);
