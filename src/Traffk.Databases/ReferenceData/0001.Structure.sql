d638f30392b7ab2d5b17e5e36b0ec131756f3632
Jason <jason@jasonthomas.com>
2017-08-27 18:16:08 -0400

    Reorg some of the ETL packages

---
+++ src/Traffk.Databases/ReferenceData/0001.Structure.sql
@@ -0,0 +1,14 @@
+﻿create table dbo.Zips
+(
+	ZipId int not null identity primary key,
+	ParentZipId int null references Zips(ZipId),
+	ZipFormat dbo.DeveloperName not null,
+	CountryId int not null,
+	ZipCode nvarchar(20) not null,
+)
+
+GO
+
+create unique index UX_ZipCode on dbo.Zips(CountryId, ZipCode)
+
+GO
---
+++ src/Traffk.Databases/ReferenceData/0100.Relationships.sql
@@ -0,0 +1,3 @@
+﻿alter table dbo.Zips Add foreignKey CountryId references [ISO3166].[Countries](CountryId)
+ 
+GO
