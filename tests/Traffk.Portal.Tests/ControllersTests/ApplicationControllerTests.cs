2998302b10ff005353101148548e5391121f1c58
Darren Alfonso <darren@traffk.com>
2017-09-13 15:28:52 -0700

    Revert "HIP-299 Remove Portal.Tests and add dummy version"
    This reverts commit 04fe846f6b10461efb71b5904a67a3be0e2f7c5e.

---
+++ tests/Traffk.Portal.Tests/ControllersTests/ApplicationControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Traffk.Bal.Services;
+using TraffkPortal.Services;
+using Microsoft.Extensions.Logging;
+using Traffk.Bal.Data.Rdb.TraffkTenantModel;
+
+namespace Traffk.Portal.Tests.ControllersTests
+{
+    [TestClass]
+    public class ApplicationControllerTests
+    {
+        [TestClass]
+        public class ApplicationControllerMocks
+        {
+            public Mock<BlobStorageServices> MockBlobStorage { get; set; }
+            public Mock<TraffkTenantModelDbContext> MockRdbContext { get; set; }
+            public Mock<CurrentContextServices> MockCurrentContextServices { get; set; }
+            public Mock<ILoggerFactory> MockLoggerFactory { get; set; }
+
+            public ApplicationControllerMocks()
+            {
+                //var mockITraffkTenantFinder = new Mock<ITraffkTenantFinder>();
+
+                //var mockIHostingEnvironment = new Mock<IHostingEnvironment>();
+                //var mockConfigStringFormatter = new Mock<ConfigStringFormatter>(mockIHostingEnvironment.Object, mockITraffkTenantFinder.Object);
+
+                //var mockDbContextOptions = new DbContextOptions<TraffkRdbContext>();
+                //MockRdbContext = new Mock<TraffkRdbContext>(mockDbContextOptions, mockITraffkTenantFinder.Object, mockConfigStringFormatter.Object);
+
+                //var mockCurrentTenantServices = new Mock<CurrentTenantServices>(mockITraffkTenantFinder.Object, MockRdbContext.Object);
+                //var mockCurrentUser = new Mock<ICurrentUser>();
+                //var mockBlobStorageServiceOptions = new Mock<IOptions<BlobStorageServicesOptions>>();
+                //mockBlobStorageServiceOptions.Setup(x => x.Value).Returns(
+                //    new BlobStorageServicesOptions
+                //    {
+                //        ConnectionString = "MockBlobStorageConnectionString"
+                //    }
+                //);
+                
+                //MockBlobStorage = new Mock<BlobStorageServices>(mockCurrentTenantServices.Object, mockCurrentUser.Object, mockBlobStorageServiceOptions.Object);
+                //MockCurrentContextServices = new Mock<CurrentContextServices>();
+                //MockLoggerFactory = new Mock<ILoggerFactory>();
+            }
+        }
+
+        [TestClass]
+        public class PortalSettingsMethodTests
+        {
+            [TestMethod]
+            public void WhenGivenNonImageFileDoNotUploadFavicon()
+            {
+                ////Default mocks
+                //var mocks = new ApplicationControllerMocks();
+                //var testController = new ApplicationController(mocks.MockBlobStorage.Object,
+                //    mocks.MockRdbContext.Object,
+                //    mocks.MockCurrentContextServices.Object,
+                //    mocks.MockLoggerFactory.Object);
+
+                //var testApplication = new Application();
+                //var mockApplicationDbset = DbSetMock.Create(testApplication);
+                //mocks.MockRdbContext.Setup(x => x.Applications).Returns(mockApplicationDbset.Object);
+                //mocks.MockRdbContext.Setup(x => x.Update(It.IsAny<object>()));
+                //mocks.MockRdbContext.Setup(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()));
+
+                //mocks.MockBlobStorage.Setup(x => x.StoreFileAsync(It.IsAny<bool>(),
+                //    It.IsAny<Roots>(),
+                //    It.IsAny<IFormFile>(),
+                //    It.IsAny<string>(),
+                //    It.IsAny<bool>()))
+                //    .Returns(new Task<Uri>(() => new Uri("http://traffk.com")));
+
+                //var testViewModel = new PortalOptionsModel();
+                //string folder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"/TestFiles/";
+                //string testFileName = "TestExcel.xlsx";
+                //string testFilePath = folder + testFileName;
+
+                //FileStream fileStream = new FileStream(testFilePath, FileMode.Open);
+
+                //Mock<IFormFile> uploadedFile = new Mock<IFormFile>();
+                //uploadedFile.Setup(f => f.Length).Returns(Convert.ToInt16(fileStream.Length));
+                //uploadedFile.Setup(f => f.FileName).Returns(testFileName);
+                //uploadedFile.Setup(f => f.OpenReadStream()).Returns(fileStream);
+
+                //testViewModel.FaviconFile = uploadedFile.Object;
+
+                //var testResult = testController.PortalSettings(1, testViewModel);
+
+            }
+        }
+    }
+}
---
+++ tests/Traffk.Portal.Tests/MockEnvironment.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using RevolutionaryStuff.Core;
+using Traffk.Portal.Tests.Helpers;
+using TraffkPortal;
+
+namespace Traffk.Portal.Tests
+{
+    public static class MockEnvironment
+    {
+        private static readonly string TokenAuthRoute = "/api/token/authenticate";
+        private static HttpClient HttpClient = null;
+        private static string AccessToken_p = null;
+
+        public static HttpClient TestClient
+        {
+            get
+            {
+                if (HttpClient == null)
+                {
+                    var testFixture = new TestFixture<Startup>();
+                    HttpClient = testFixture.Client;
+                    var accessToken = GetBearerToken(HttpClient, @"darren@traffk.com", Guid.Parse("58f620c5-0669-44dc-a2fa-d79a3df80103"));
+                    HttpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {accessToken}");
+                }
+
+                return HttpClient;
+            }
+
+        }
+
+
+        public static string AccessToken
+        {
+            get { return AccessToken_p; }
+        }
+
+
+        public static string GetBearerToken(HttpClient client, string username, string password)
+        {
+            if (String.IsNullOrEmpty(AccessToken_p))
+            {
+                var tokenRequestFormContent = new Dictionary<string, string>
+                {
+                    ["username"] = username,
+                    ["password"] = password
+                };
+                var content = new FormUrlEncodedContent(tokenRequestFormContent);
+
+                var response = client.PostAsync(TokenAuthRoute, content).ExecuteSynchronously();
+
+                var json = response.Content.ReadAsStringAsync().ExecuteSynchronously();
+                var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(json);
+
+                AccessToken_p = tokenResponse.access_token;
+            }
+
+            return AccessToken_p;
+        }
+
+        public static string GetBearerToken(HttpClient client, string username, Guid apiKey)
+        {
+            if (String.IsNullOrEmpty(AccessToken_p))
+            {
+                var tokenRequestFormContent = new Dictionary<string, string>
+                {
+                    ["username"] = username,
+                    ["apiKey"] = apiKey.ToString()
+                };
+                var content = new FormUrlEncodedContent(tokenRequestFormContent);
+
+                var response = client.PostAsync(TokenAuthRoute, content).ExecuteSynchronously();
+
+                var json = response.Content.ReadAsStringAsync().ExecuteSynchronously();
+                var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(json);
+
+                AccessToken_p = tokenResponse.access_token;
+            }
+
+            return AccessToken_p;
+        }
+    }
+
+    public class TokenResponse
+    {
+        public string access_token { get; set; }
+    }
+}
